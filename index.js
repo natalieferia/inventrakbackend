2fafb1b4be0d51e277f505327aeb6887644cd830
Natalie <natalieferia1@gmail.com>
2024-12-10 22:15:55 -0500

    solucion

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(session({
     resave: true,
     saveUninitialized: true,
     cookie: {
-        sameSite: "lax"
+        sameSite: "none"
         //httpOnly: true,
         //secure: true,
         //maxAge: 24 * 60 * 60 * 1000
